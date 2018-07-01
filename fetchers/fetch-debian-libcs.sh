49b4395a5e263defcdcd6eab79c6c98b457b139c
Francesco Cagnin <francesco.cagnin@gmail.com>
2018-07-01 14:46:36 +0200

    Update to match ubuntu's fetcher

--- fetchers/fetch-debian-libcs.sh
+++ fetchers/fetch-debian-libcs.sh
@@ -6,38 +6,37 @@ mkdir -p "$WORKDIR"
 cd "$WORKDIR"
 
 for DISTRO in "jessie" "wheezy" "stretch"; do
-    pushd . >/dev/null 2>&1
-    WORKDIR="$(pwd)/$DISTRO"
-    mkdir -p "$WORKDIR"
-    cd "$WORKDIR"
-        DEB_URLS=""
-        for ARCH in "i386" "amd64"; do
-            DEB_URLS+=" $(wget -O - "https://packages.debian.org/$DISTRO/$ARCH/libc6/download" 2>/dev/null \
-                | grep -o -m 1 "http://[^\"]*libc6[^\"]*.deb")"
-        done
-        set -- junk $DEB_URLS
+    for ARCH in "i386" "amd64"; do
+        pushd . >/dev/null 2>&1
+        WORKDIR="$(pwd)/$DISTRO"
+        mkdir -p "$WORKDIR"
+        cd "$WORKDIR"
+            DEB_URLS="$(wget -O - "https://packages.debian.org/$DISTRO/$ARCH/libc6/download" 2>/dev/null | grep -o -m 1 "http://[^\"]*libc6[^\"]*.deb")"
+            for DEB_URL in $DEB_URLS; do
+                echo "Fetching: $DEB_URL"
+                DEB_FILENAME="$(basename "$DEB_URL")"
+                if [[ $DEB_FILENAME =~ libc6_(.*)_$ARCH.deb ]]; then
+                    VERS="${BASH_REMATCH[1]}"
 
-        for DEB_URL in $DEB_URLS; do
-            DEB_FILENAME="$(basename "$DEB_URL")"
-            if [[ $DEB_FILENAME =~ libc6_(.*)_$ARCH.deb ]]; then
-                VERS="${BASH_REMATCH[1]}"
+                    LIBC_FILENAME="libc-$ARCH-$VERS.so"
+                    LD_FILENAME="ld-$ARCH-$VERS.so"
+                    if [[ ( ! -f $LIBC_FILENAME ) || ( ! -f $LD_FILENAME ) ]]; then
+                        echo "Processing: $DEB_FILENAME"
 
-                LIBC_FILENAME="libc-$ARCH-$VERS.so"
-                LD_FILENAME="ld-$ARCH-$VERS.so"
-                if [[ ( ! -f $LIBC_FILENAME ) || ( ! -f $LD_FILENAME ) ]]; then
-                    echo "Fetching: "$DEB_FILENAME""
-
-                    pushd . >/dev/null 2>&1
-                    TEMPDIR="$(mktemp -d)"
-                    cd "$TEMPDIR"
-                        wget "$DEB_URL" 2>/dev/null
-                        if ar x "$DEB_FILENAME" && tar xf data.tar.?z; then
-                            mv "$(realpath $(find "$TEMPDIR" -name "libc.so.6"))" "$WORKDIR/$LIBC_FILENAME"
-                            mv "$(realpath $(find "$TEMPDIR" -name "ld-*.so"))" "$WORKDIR/$LD_FILENAME"
-                        fi
-                    popd >/dev/null 2>&1
+                        pushd . >/dev/null 2>&1
+                        TEMPDIR="$(mktemp -d)"
+                        cd "$TEMPDIR"
+                            wget "$DEB_URL" 2>/dev/null
+                            if ar x "$DEB_FILENAME" && tar xf data.tar.?z; then
+                                mv "$(realpath $(find "$TEMPDIR" -name "libc.so.6"))" "$WORKDIR/$LIBC_FILENAME"
+                                mv "$(realpath $(find "$TEMPDIR" -name "ld-*.so"))" "$WORKDIR/$LD_FILENAME"
+                            fi
+                        popd >/dev/null 2>&1
+                    else
+                        echo "Skipping: $DEB_FILENAME"
+                    fi
                 fi
-            fi
-        done
-    popd >/dev/null 2>&1
+            done
+        popd >/dev/null 2>&1
+    done
 done
