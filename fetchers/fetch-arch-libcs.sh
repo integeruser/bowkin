2283110a19772bec02b90c706b35d821f61a81db
Francesco Benvenuto <francesco_benvenuto@me.com>
2018-07-05 19:01:09 +0200

    Fix illegal character

--- fetchers/fetch-arch-libcs.sh
+++ fetchers/fetch-arch-libcs.sh
@@ -10,7 +10,7 @@ for ARCH in "i686" "x86_64"; do
     PKG_URLS="$PKG_URLS $(wget "https://archive.archlinux.org/packages/g/glibc/" 2>&1 \
         --spider --no-clobber --no-directories --no-parent --recursive --level=1 --execute="robots=off" \
         --accept "glibc-*-$ARCH.pkg.tar.xz" --reject "*.sig" \
-        | grep -Eo http.+\.pkg\.tar\.xz)"
+        | grep -Eo http.+\.pkg\.tar\.xz || true)"
 done
 set -- junk $PKG_URLS
 
@@ -19,8 +19,9 @@ for PKG_URL in $PKG_URLS; do
     if [[ $PKG_FILENAME =~ glibc-(.*)-$ARCH.pkg.tar.xz ]]; then
         VERS="${BASH_REMATCH[1]}"
 
-        LIBC_FILENAME="libc-$ARCH-$VERS.so"
-        LD_FILENAME="ld-$ARCH-$VERS.so"
+        LIBC_FILENAME=$(sed -E 's/[^a-zA-Z0-9_\.\-]/_/g' <<< "libc-$ARCH-$VERS.so")
+        LD_FILENAME=$(sed -E 's/[^a-zA-Z0-9_\.\-]/_/g' <<< "ld-$ARCH-$VERS.so")
+        
         if [[ ( ! -f $LIBC_FILENAME ) || ( ! -f $LD_FILENAME ) ]]; then
             echo "Processing: "$PKG_FILENAME""
 
